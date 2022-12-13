1e74177fe886a0444e65dbea09ce835972855d71
Ebuka <93505445+ebukaobiajulu@users.noreply.github.com>
2022-12-13 13:23:19 +0100

    Updated files

---
+++ main.js
@@ -0,0 +1,4 @@
+// const  sections
+const allSections = document.querySelector('.main-content');
+const sectBtns = document.querySelectorAll('.list');
+const sectBtn = document.querySelectorAll('.list_item')
